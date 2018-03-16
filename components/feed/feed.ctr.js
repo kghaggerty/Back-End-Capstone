962bc02301f38b7bce8f1325d69d799587da8b65
Kevin Haggerty <kghaggerty@gmail.com>
2018-03-16 11:57:04 -0500

    user feed is now limited to certain number of messages. order newest to oldest

--- components/feed/feed.ctr.js
+++ components/feed/feed.ctr.js
@@ -1,22 +1,22 @@
 angular.module("authApp")
     .controller("feedController", function ($scope, $location, feedFactory) {
-        //Get news feed to display to user
-        // feedFactory.getUserFeed().then(res => {
-        //     console.log(res, "user feed result")
-        // })
         $scope.feed = []
 
+        let updateFeed = function () {
+        feedFactory.getUserFeed().then(feedData => {
+            feedData.data.forEach(eachFeed => {
+                $scope.feed.push(eachFeed)
+            })
+        })
+    }
+    
+    updateFeed();
+        
         //Send news feed form to database
         $scope.postFeed = function (auth) {
             feedFactory.postUserFeed(auth).then(res => {
-                feedFactory.getUserFeed().then(feedData => {
-                    feedData.data.forEach(eachFeed => {
-                        $scope.feed.push(eachFeed)
-                        console.log(eachFeed, "user feed loop")
-                    })
-                })
+                updateFeed();
             })
         }
-
-        console.log($scope.feed, "scope.feed")
+        
     })
