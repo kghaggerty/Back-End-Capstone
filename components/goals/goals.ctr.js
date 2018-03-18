8e81745f53081abb10c71e90c11d4b96f7384715
Kevin Haggerty <kghaggerty@gmail.com>
2018-03-18 16:51:55 -0500

    user can post and see their goals on cards

--- components/goals/goals.ctr.js
+++ components/goals/goals.ctr.js
@@ -1,19 +1,23 @@
 angular.module("authApp")
     .controller("goalsController", function ($scope, $location, goalsFactory) {
+        $scope.goals = []
+        
 
-        // $scope.todoList = [{ todoText: 'Clean House', done: false }]
+        //Get user goals from database
+        let updateGoals = function () {
+            goalsFactory.getGoals().then(feedData => {
+                feedData.data.forEach(eachFeed => {
+                    $scope.goals.push(eachFeed)
+                    console.log(eachFeed)
+                })
+            })
+        }
 
-        // $scope.todoAdd = function () {
-        //     $scope.todoList.push({ todoText: $scope.todoInput, done: false })
-        //     $scope.todoInput = ""
-        // }
-
-        // $scope.remove = function () {
-        //     var oldList = $scope.todoList
-        //     $scope.todoList = []
-        //     angular.forEach(oldList, function (x) {
-        //         if (!x.done) $scope.todoList.push(x)
-        //     })
-        // }
-
+        //Send goal input to database
+        $scope.goalInput = function (auth) {
+            goalsFactory.postGoals(auth).then(res => {
+                console.log(res, "goal")
+            })
+            updateGoals();
+        }
     })
--- components/goals/goals.factory.js
+++ components/goals/goals.factory.js
@@ -3,13 +3,22 @@ angular
     .factory("goalsFactory", function ($http, $routeParams) {
         //Making api call to goals Controller
         return Object.create(null, {
-            "postCheckIn": {
+            "postGoals": {
                 value: function (auth) {
                     return $http({
-                        "url": "http://localhost:5000/api/DailyCheck",
+                        "url": "http://localhost:5000/api/Goals",
                         "method": "POST",
                         "Accepts": "application/json",
                         "data": auth
+                    })
+                }
+            },
+            "getGoals": {
+                value: function (auth) {
+                    return $http({
+                        "url": "http://localhost:5000/api/Goals",
+                        "method": "GET",
+                        "Accepts": "application/json",
                     })
                 }
             }
