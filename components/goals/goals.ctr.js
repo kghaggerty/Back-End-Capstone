7741bbc761b0e502c1d27f31e82ff3ee32d10fd4
Kevin Haggerty <kghaggerty@gmail.com>
2018-03-20 12:05:01 -0500

    goals returns current user goals

--- components/goals/goals.ctr.js
+++ components/goals/goals.ctr.js
@@ -2,13 +2,12 @@ angular.module("authApp")
     .controller("goalsController", function ($scope, $location, goalsFactory) {
         $scope.goals = []
         
-
+        
         //Get user goals from database
-        let updateGoals = function () {
+        $scope.updateGoals = function () {
             goalsFactory.getGoals().then(feedData => {
                 feedData.data.forEach(eachFeed => {
                     $scope.goals.push(eachFeed)
-                    console.log(eachFeed)
                 })
             })
         }
@@ -16,8 +15,8 @@ angular.module("authApp")
         //Send goal input to database
         $scope.goalInput = function (auth) {
             goalsFactory.postGoals(auth).then(res => {
-                console.log(res, "goal")
+                $scope.auth = {}
             })
-            updateGoals();
+            // updateGoals(auth);
         }
     })
