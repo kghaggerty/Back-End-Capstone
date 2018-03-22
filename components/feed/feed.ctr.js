d5f79623de2eccc0b6072204e1d5eaa656023b9f
Kevin Haggerty <kghaggerty@gmail.com>
2018-03-22 13:12:05 -0500

    remigrate

--- components/feed/feed.ctr.js
+++ components/feed/feed.ctr.js
@@ -1,23 +1,21 @@
 angular.module("authApp")
     .controller("feedController", function ($scope, $location, feedFactory) {
-        $scope.feed = []
 
     let updateFeed = function () {
+        $scope.feed = []
         feedFactory.getUserFeed().then(feedData => {
             feedData.data.forEach(eachFeed => {
                 $scope.feed.push(eachFeed)
             })
         })
     }
-    
     updateFeed();
-        
+    
     //Send news feed form to database
     $scope.postFeed = function (auth) {
         feedFactory.postUserFeed(auth).then(res => {
             $scope.auth = {}
             updateFeed();
         })
-    }
-        
+    }        
 })
